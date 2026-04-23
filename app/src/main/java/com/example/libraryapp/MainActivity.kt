591a92d248ee97f395a0d78e1afff5d5879089e3
mehmet karabalcı <mehmetkarabalci1@gmail.com>
2026-04-23 19:12:01 +0300

    add Login Screen

--- app/src/main/java/com/example/libraryapp/MainActivity.kt
+++ app/src/main/java/com/example/libraryapp/MainActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-
+import com.example.libraryapp.ui.navigation.NavGraph
 
 
 class MainActivity : ComponentActivity() {
@@ -12,7 +12,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-
+            NavGraph()
         }
     }
 }
---
+++ app/src/main/java/com/example/libraryapp/ui/navigation/Screen.kt
@@ -0,0 +1,10 @@
+package com.example.libraryapp.ui.navigation
+
+
+// Sayfa routelarının tanımı
+
+sealed class Screen (val route: String )
+{
+    object Login : Screen("login")
+    object Register : Screen("register")
+}
