c10f92f58b9a6ebdaa3e9c4b310d11ea5bdaf75d
mehmet karabalcı <mehmetkarabalci1@gmail.com>
2026-04-28 16:02:44 +0300

    auto generated build config

--- app/src/main/java/com/example/libraryapp/data/supabase/SupabaseClient.kt
+++ app/src/main/java/com/example/libraryapp/data/supabase/SupabaseClient.kt
@@ -1,12 +1,13 @@
 package com.example.libraryapp.data.supabase
 
+import com.example.libraryapp.BuildConfig
 import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 
 val supabase = createSupabaseClient(
-    supabaseKey = "", //buildCongif tanımlayarak kullanırız
-    supabaseUrl = ""
+    supabaseKey = BuildConfig.SUPABASE_ANON_KEY, //buildCongif tanımlayarak kullanırız
+    supabaseUrl = BuildConfig.SUPABASE_URL
 ) {
     install(Postgrest)
     install(Auth)
